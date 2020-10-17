8aef3aec6e46f38b5ac1d30e306982e62f328f0c
permataindah <72175766+permataindah@users.noreply.github.com>
2020-10-17 13:38:22 +0700

    Create permata.php

---
+++ language/permata.php
@@ -0,0 +1,24 @@
+<?PHP
+
+//contoh penulisan coding yang kurang baik
+
+$nama="Yasmin";
+
+if ($nama="Yasmin")
+
+{
+
+echo"Kamu adalah $nama";
+
+}
+
+else
+
+{
+
+echo"kamu buka $nama";
+
+}
+
+?>
+
